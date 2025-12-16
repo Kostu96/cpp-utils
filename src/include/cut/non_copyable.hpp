6152d4e74f2018dc4cecf488064cf289ef14b5d1
Konstanty <kostu96@gmail.com>
2025-12-16 18:18:21 +0100

    Fix moving NonCopyable

--- src/include/cut/non_copyable.hpp
+++ src/include/cut/non_copyable.hpp
@@ -8,6 +8,9 @@ protected:
 
     NonCopyable(const NonCopyable&) = delete;
     NonCopyable& operator=(const NonCopyable&) = delete;
+
+    NonCopyable(NonCopyable&&) = default;
+    NonCopyable& operator=(NonCopyable&&) = default;
 };
 
 } // namespace cut
